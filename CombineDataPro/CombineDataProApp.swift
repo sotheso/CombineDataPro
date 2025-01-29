4d62a85c85623d12dbabf87b8cbe63880c1e7774
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-29 13:59:50 +0330

    Initial Commit

---
+++ CombineDataPro/CombineDataProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDataProApp.swift
+//  CombineDataPro
+//
+//  Created by Sothesom on 10/11/1403.
+//
+
+import SwiftUI
+
+@main
+struct CombineDataProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineDataPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineDataPro
+//
+//  Created by Sothesom on 10/11/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
