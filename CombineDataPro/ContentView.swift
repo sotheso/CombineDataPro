cb667991d63a8e97ea34ca240bb09614c8cdda41
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-02-01 21:51:40 +0330

    Coding our ProfileRow: ساخت قسمت پروفایل

--- CombineDataPro/ContentView.swift
+++ CombineDataPro/ContentView.swift
@@ -41,6 +41,7 @@ struct ContentView: View {
     var content: some View {
         VStack{
             
+            ProfileRow()
             // other content
             
 // Table View FAQ...
@@ -60,12 +61,7 @@ struct ContentView: View {
                     MenuRow(title: "YouTube Channel", leftIcon: "play.rectangle.fill", rightIcon: "link")
                 })
             }
-            .padding(16)
-            .background(Color("Background 1"))
-            .background(VisualEffectBlur(blurStyle: .systemUltraThinMaterialDark ))
-            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.white, lineWidth: 1).blendMode(.overlay))
-            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
-            .padding(.top, 20)
+            .blurBacground()
             
             Text("Version : 1.0.0")
                 .font(.footnote)
--- CombineDataPro/Utile/Extentions.swift
+++ CombineDataPro/Utile/Extentions.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 extension View {
-    // ویوی دکمه "Contact Support"
-    // برای دارک مود
+// ویوی دکمه "Contact Support"
+// برای دارک مود
     func angularGradientGlow(colors: [Color]) -> some View {
         self.overlay(AngularGradient(gradient: Gradient(colors: colors), center: .center, angle: .degrees(0.0)))
             .mask(self)
     }
-    
     func linerGradientBackground(colors: [Color]) -> some View {
         self.overlay(LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing))
             .mask(self)
+    }
+    
+    
+// برای افکت بلر پروفایل ویو
+    func blurBacground() -> some View {
+        self.padding(16)
+            .background(Color("Background 1"))
+            .background(VisualEffectBlur(blurStyle: .systemThinMaterialDark))
+            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.white, lineWidth: 1.0).blendMode(.overlay))
+            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
     }
 }
